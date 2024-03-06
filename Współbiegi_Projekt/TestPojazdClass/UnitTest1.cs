08dc5b759ed11fe28921c4719f5f1e6b13c45a72
aleks <abidingbrush787@gmail.com>
2024-03-06 17:03:53 +0100

    testy2

---
+++ Współbiegi_Projekt/TestPojazdClass/UnitTest1.cs
@@ -0,0 +1,42 @@
+using KlasaPojazd;
+
+namespace TestPojazdClass
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            [TestMethod]
+            void Opis_ZwracaPoprawnyOpisPojazdu()
+            {
+                // Arrange
+                var pojazd = new Pojazd("Samochód", 120);
+
+                // Act
+                var opis = pojazd.Opis();
+
+                // Assert
+                Assert.AreEqual("Pojazd Samochód mo¿e jechaæ z maksymaln¹ prêdkoœci¹ 120 km/h.", opis);
+            }
+        }
+
+        [TestClass]
+        public class SamochodTests
+        {
+            [TestMethod]
+            public void Opis_ZwracaPoprawnyOpisSamochodu()
+            {
+                // Arrange
+                var samochod = new Samochod("Sedan", 180, 4);
+
+                // Act
+                var opis = samochod.Opis();
+
+                // Assert
+                Assert.AreEqual("Pojazd Sedan mo¿e jechaæ z maksymaln¹ prêdkoœci¹ 180 km/h. Ma 4 drzwi.", opis);
+            }
+        }
+    }
+}
